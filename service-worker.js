d1705ba2e2d3f880c9b9edfb4451adf4ce695ee3
DmitriyOrlov <orlov.dmitriy2303@outlook.com>
2019-10-11 00:28:16 +0300

    исправлен старый баг с незагрузкой карты

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/giglevich/precache-manifest.732586d9ce99d519c599286a409b29b1.js"
+  "/giglevich/precache-manifest.0fb69dff918a2e64f3a2f58073c8cd20.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "spa"});
